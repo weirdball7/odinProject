2e23ca6d4b301bbf26fa94e0f629a96e10ef568c
weirdball7 <moteamitay04@gmail.com>
2024-07-06 21:57:07 +0300

    lerned how to use the html-webpack-plugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,25 @@
 import _ from 'lodash';
-import myName from './myName';
+import './style.css'
+import Icon from './bulbul.jpg'
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.textContent = myName("Rotem");
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+  
     return element;
   }
   
   document.body.appendChild(component());
-  
